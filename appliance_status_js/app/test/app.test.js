f3fe13100f42209771626e8c799c7be41e7b4e49
Patrick Gerken <do3cc@patrick-gerken.de>
2021-09-05 18:37:13 +0200

    test: Add more tests
    Finished writing tests for the first method.
    The second and last method did not get a proper test.
    I would need to investigate a lot more time into sinon to mock
    requests and this seems not to be time well spent. Should have
    tried vue or similar and then have (hopefully) testable components.

--- appliance_status_js/app/test/app.test.js
+++ appliance_status_js/app/test/app.test.js
@@ -4,11 +4,23 @@ import * as jQuery from "jquery-jsdom";
 
 describe("extendButtonBehavior", function(){
     it("disables the button", function(){
-        const jq = jQuery("<form><button></form>");
+        const jq = jQuery("<div><form><button/></form></div>");
         const button = jq.find('button');
         const form = jq.find('form');
         expect(extendButtonBehavior(button, form).attr('disabled')).to.equal("disabled");
-
-
+    });
+    it("reenables the button upon change", function(){
+        const jq = jQuery("<div><form><button/></form></div>");
+        const button = jq.find('button');
+        const form = jq.find('form');
+        extendButtonBehavior(button, form);
+        button.trigger("change");
+        expect(button.attr('disabled')).to.equal(undefined);
     })
+})
+
+describe("extendFormBehavior", function(){
+    it("does things that are unreasonable to test in nodejs", function(){
+        // I see the light now. No more jquery. 
+    });
 })
